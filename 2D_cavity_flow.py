28367833caeb860841f9a8eff57e2112921eb3c1
Francesco <ambrogifra@gmail.com>
2020-04-21 12:48:50 -0400

    Finished 2D cavity flow and added som plotting features

--- 2D_cavity_flow.py
+++ 2D_cavity_flow.py
@@ -3,10 +3,10 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D   # New library required for 3D plots
 
-nx = 41
-ny = 41
-nt = 500
-nit = 50
+nx = 81
+ny = 81
+nt = 700
+nit = 300
 c = 1
 dx = 2 / (nx - 1)
 dy = 2 / (ny - 1)
@@ -22,21 +22,116 @@ u = np.zeros((ny,nx))
 v = np.zeros((ny,nx))
 p = np.zeros((ny,nx))
 b = np.zeros((ny,nx))
-nt = 100   # Number of iterations for the Poisson solver
+
+# In this function we define the rhs of poisson equation for p
+def build_rhs_poisson(b,rho,dt,u,v,dx,dy):
+    
+    b[1:-1, 1:-1] = (rho * (1 / dt * 
+                    ((u[1:-1, 2:] - u[1:-1, 0:-2]) / 
+                     (2 * dx) + (v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy)) -
+                    ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx))**2 -
+                      2 * ((u[2:, 1:-1] - u[0:-2, 1:-1]) / (2 * dy) *
+                           (v[1:-1, 2:] - v[1:-1, 0:-2]) / (2 * dx))-
+                          ((v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy))**2))
+    return b
+
+# In this function the Poisson for pressure is solved
+# The presence of the pseudo-time variable nit.
+# This step will generate a divergence free velocity field
+    
+def poisson_solve(p, dx, dy, b):
+    pn = np.empty_like(p)
+    pn = p.copy()
+    
+    for k in range(nit):
+        pn = p.copy()
+        p[1:-1, 1:-1] = (((pn[1:-1, 2:] + pn[1:-1, 0:-2]) * dy**2 + 
+                          (pn[2:, 1:-1] + pn[0:-2, 1:-1]) * dx**2) /
+                          (2 * (dx**2 + dy**2)) -
+                          dx**2 * dy**2 / (2 * (dx**2 + dy**2)) * 
+                          b[1:-1,1:-1])
+        # Set the boundary conditions for Poisson eq
+        p[:, -1] = p[:, -2] # dp/dx = 0 at x = 2
+        p[0, :] = p[1, :]   # dp/dy = 0 at y = 0
+        p[:, 0] = p[:, 1]   # dp/dx = 0 at x = 0
+        p[-1, :] = 0        # p = 0 at y = 2
+    return p
 
 # Here we call the function that solve the cavity flow
-u,v,p = cavity(nt,u,dt,dx,dy,p,rho,nu)
+# Where the rest of the flow equation are solved
+def cavity(nt, u, v, dt, dx, dy, p, rho, nu):
+    un = np.empty_like(u)
+    vn = np.empty_like(v)
+    b = np.zeros((ny, nx))
 
-fig = plt.figure(figsize(22,14),dpi=100)
+    for n in range(nt):
+        un = u.copy()
+        vn = v.copy()
+        
+        b = build_rhs_poisson(b, rho, dt, u, v, dx, dy)
+        p = poisson_solve(p, dx, dy, b)
+        
+        u[1:-1, 1:-1] = (un[1:-1, 1:-1]-
+                         un[1:-1, 1:-1] * dt / dx *
+                        (un[1:-1, 1:-1] - un[1:-1, 0:-2]) -
+                         vn[1:-1, 1:-1] * dt / dy *
+                        (un[1:-1, 1:-1] - un[0:-2, 1:-1]) -
+                         dt / (2 * rho * dx) * (p[1:-1, 2:] - p[1:-1, 0:-2]) +
+                         nu * (dt / dx**2 *
+                        (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, 0:-2]) +
+                         dt / dy**2 *
+                        (un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1])))
+
+        v[1:-1,1:-1] = (vn[1:-1, 1:-1] -
+                        un[1:-1, 1:-1] * dt / dx *
+                       (vn[1:-1, 1:-1] - vn[1:-1, 0:-2]) -
+                        vn[1:-1, 1:-1] * dt / dy *
+                       (vn[1:-1, 1:-1] - vn[0:-2, 1:-1]) -
+                        dt / (2 * rho * dy) * (p[2:, 1:-1] - p[0:-2, 1:-1]) +
+                        nu * (dt / dx**2 *
+                       (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, 0:-2]) +
+                        dt / dy**2 *
+                       (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1])))
+
+        u[0, :]  = 0
+        u[:, 0]  = 0
+        u[:, -1] = 0
+        u[-1, :] = 1    # set velocity on cavity lid equal to 1
+        v[0, :]  = 0
+        v[-1, :] = 0
+        v[:, 0]  = 0
+        v[:, -1] = 0
+        
+        
+    return u, v, p
+
+        
+u,v,p = cavity(nt, u, v, dt, dx, dy, p, rho, nu)
+
+fig = plt.figure(1, figsize=(22,14),dpi=100)
 # Let us plot the pressure field as a contour
 plt.contourf(X,Y,p,alpha=0.5,cmap=cm.coolwarm)
-plt.colorbar(surf, shrink=0.5, aspect=10)
+plt.colorbar()
 # Let us plot the pressure field outlines
+
 plt.contour(X,Y,p,cmap=cm.coolwarm)
 
 # Here let us plot the velocity field
 plt.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2])
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.show()
 
+# Plot the velocity profile u and v at the center of the cavity
+fig = plt.figure(2, figsize=(22,14), dpi=100)
+plt.plot(u[:,40], y, '-ok', lw=2)
+plt.autoscale(tight=True)
+plt.xlabel('U')
+plt.ylabel('Y')
+
+fig = plt.figure(3, figsize=(22,14), dpi=100)
+plt.plot(x, v[40,:], '-ok', lw=2)
+plt.autoscale(tight=True)
+plt.xlabel('X')
+plt.ylabel('V')
+
+plt.show()
